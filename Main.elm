4ae7405a48f11f46c09e96ecada128ed2b6e5a03
Mike Davies <miked@antfx.com>
2016-08-26 11:54:29 +0200

    Comments

--- Main.elm
+++ Main.elm
@@ -17,6 +17,17 @@ main =
 
 
 -- MODEL
+{-
+
+   Elm uses a single point of knowledge, that means that this is the only real user
+   data in the entire application, so we have to store any sub-components's data
+   in this Model.
+
+   So as well as our own model we also define an instance of SomeComponent.Model
+   that we use to store any data required by SomeComponent. We will use it to
+   generate SomeComponent's view later on
+
+-}
 
 
 type alias Model =
@@ -52,6 +63,54 @@ type Msg
     | SomeComponentMsg SomeComponent.Msg
 
 
+
+-- VIEW
+{-
+   Here view makes a call to SomeComponent.view passing in the instance of SomeComponent.Model
+   from it's own model defined above.
+
+    Problem: Main.view returns Html Msg but SomeComponent.view returns Html SomeComponent.Msg
+    which are not compatible types.
+
+    So we use the Html.map function to 'map' SomeComponent.Msg
+    to a message defined in Main.Msg, in this case we added a new message called SomeComponentMsg
+
+    Now, when any messages are generated in SomeComponent they are sent to Main.update as a
+    SomeComponentMsg and the SomeComponent.Msg is given as the 'payload' (the param to the message)
+-}
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text "This is the main Component" ]
+        , button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (toString model.count) ]
+        , button [ onClick Increment ] [ text "+" ]
+        , Html.map SomeComponentMsg (SomeComponent.view model.someComponent)
+        ]
+
+
+
+{-
+
+   As well as handling its own messages, Main.update now handles messages generated
+   from SomeComponent via the SomeComponentMsg message.
+
+   When one of these messages is received we also get the real SomeComponent.Msg
+   as the argument (payload). So we call SomeComponent.update passing in the
+   model from Main.Model and the real message
+
+   That returns a new instance of SomeComponent.Model that we update in our instance
+   of Main.Model and a SomeComponent.Cmd
+
+   Again, SomeComponent.Cmd is not compatible with Main.Cmd which Main.update needs to
+   return, so we call Cmd.map to 'map' the SomeComponent.Msg to Main.Msg just like we
+   did with Html.map above
+
+-}
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
@@ -67,18 +126,3 @@ update msg model =
                     SomeComponent.update message model.someComponent
             in
                 ( { model | someComponent = newModel }, Cmd.map SomeComponentMsg cmd )
-
-
-
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ h1 [] [ text "This is the main Component" ]
-        , button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (toString model.count) ]
-        , button [ onClick Increment ] [ text "+" ]
-        , Html.map SomeComponentMsg (SomeComponent.view model.someComponent)
-        ]
